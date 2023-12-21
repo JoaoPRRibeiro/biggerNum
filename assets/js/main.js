da402092390fc2536258fc1cf0a1203d194d645b
JoaoPRRibeiro <140761566+JoaoPRRibeiro@users.noreply.github.com>
2023-12-21 10:59:22 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,13 @@ function biggerNum() {
   const text = document.getElementById("text");
 
   if (num1 > num2) {
-    text.innerText = `${num1} maior que ${num2}`;
+    text.innerText = `${num1} bigger than ${num2}`;
   } else if (num2 > num1) {
-    text.innerText = `${num2} maior que ${num1}`;
+    text.innerText = `${num2} bigger than ${num1}`;
   } else {
-    text.innerText = `Os numeros sao iguais`;
+    text.innerText = `The numbers are equal`;
   }
+
+  document.getElementById("num1").value = ``;
+  document.getElementById("num2").value = ``;
 }
